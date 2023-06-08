bee4116e3c7e36ec155e7942c1b4a9bab8913505
Hanayama-Kayo <testcomboom@gmail.com>
2023-06-08 12:34:52 +0500

    2.0 vers

--- js/main.js
+++ js/main.js
@@ -13,10 +13,12 @@ window.addEventListener("scroll", function () {
   }
   function openNavbar() {
   document.getElementById("navbar-responsive").style.right = "0";
+  document.getElementById("down2").style.opacity = "0"
   // document.getElementById("navbar-responsive").style.top = "0";
 }
 function closeNavbar() {
   document.getElementById("navbar-responsive").style.right = "-100%";
+  document.getElementById("down2").style.opacity = "1"
   // document.getElementById("navbar-responsive").style.top = "-100%";
 }
 
@@ -34,5 +36,19 @@ function shrink() {
     navbar.classList.add("navbar-shrink");
   } else {
 		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+window.addEventListener("scroll", function () {
+  down();
+});
+
+let down2 = document.getElementById("down2");
+
+function down() {
+  if (scrollY > 530) {
+    down2.classList.add("down-remove");
+  } else {
+		down2.classList.remove("down-remove");
 	}
 }
